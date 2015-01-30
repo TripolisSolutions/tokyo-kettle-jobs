77588a904c42fac79e3a87dcf60d072f0cd2311c
hieu-le <duchieule@hotmail.com.vn>
2015-01-30 05:49:46 -0500

    update templates

--- lib/tokyo-kettle-jobs/jobs/import_contacts/import_contacts.rb
+++ lib/tokyo-kettle-jobs/jobs/import_contacts/import_contacts.rb
@@ -1,8 +1,24 @@
 module TokyoKettleJobs
   module Jobs
     module ImportContacts
+      def self.get_file_path path
+        File.expand_path(path, __FILE__)
+      end
+
       def self.entry_file
-        File.expand_path('../kettle/import_contacts.kjb', __FILE__)
+        get_file_path('../kettle/import_contacts.kjb')
+      end
+
+      def self.import_contacts_template_task
+        get_file_path('../kettle/import_contacts.ktr')
+      end
+
+      def self.import_contacts_validation_template_job
+        get_file_path('../kettle/import_contacts_validation.kjb')
+      end
+
+      def self.import_contacts_validation_template_task
+        get_file_path('../kettle/import_template_validation.ktr')
       end
     end
   end
