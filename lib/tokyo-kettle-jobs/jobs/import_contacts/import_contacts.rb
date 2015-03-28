35ebd58541d825bcee4d8d873dd4e251a5c6cb82
hieu-le <duchieule@hotmail.com.vn>
2015-03-28 06:49:22 -0400

    add kettle shell script

--- lib/tokyo-kettle-jobs/jobs/import_contacts/import_contacts.rb
+++ lib/tokyo-kettle-jobs/jobs/import_contacts/import_contacts.rb
@@ -24,6 +24,10 @@ module TokyoKettleJobs
       def self.s3_utility
         get_file_path('../s3_utility.rb')
       end
+
+      def self.get_kettle_shell_script kitchen_cmd, entry_file, kettle_import_contacts_job, s3_region, s3_utility, default_s3_bucket_name, import_csv_file_name, s3_kettle_path, ktr_file_name
+        "#{kitchen_cmd} -file:\"#{entry_file}\" #{kettle_import_contacts_job} -param:S3_REGION=\"#{s3_region}\" -param:S3_UTILITY=\"#{s3_utility}\" -param:IMPORT_BUCKET_NAME=\"#{default_s3_bucket_name}\" -param:IMPORT_CSV_FILENAME=\"#{import_csv_file_name}\" -param:S3_KETTLE_JOBS_PATH=\"#{s3_kettle_path}\" -param:KETTLE_FILENAME=\"#{ktr_file_name}\" & >/dev/null 2>&1"
+      end
     end
   end
 end
